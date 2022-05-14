a6ff4442cebf37e06e9c995513507e7af1597975
hardySin <deep16hardy@gmail.com>
2022-05-15 04:45:09 +0530

    un structured files removed

--- interfaces/category.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface ICategory extends Document {
-
-    category_id: string;
-    categoryName: string;
-}
--- interfaces/user.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface IUser extends Document {
-    
-    user_id:string;
-    username:string;
-    useremail:string;
-    userpassowrd:string;
-    }
